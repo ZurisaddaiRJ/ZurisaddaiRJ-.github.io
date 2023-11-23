907982c24a8c00eafb64ce41d4c2a6c16c637263
ZurisaddaiRJ <zuleyma.juarez_2002@hotmail.com>
2023-11-23 13:44:02 -0600

    Agregar curso

--- src/app/certificates/certificates.component.ts
+++ src/app/certificates/certificates.component.ts
@@ -38,17 +38,22 @@ export class CertificatesComponent implements OnInit {
 
     let cert6 = {
       titulo: "Python",
-      descripcion: "https://www.kaggle.com/learn/certification/zurisaddaireyesjurez/python"
+      descripcion: "Certificado por la realizaciòn del curso PYTHON: https://www.kaggle.com/learn/certification/zurisaddaireyesjurez/python"
     };
 
     let cert7 = {
       titulo: "Pandas",
-      descripcion: "https://www.kaggle.com/learn/certification/zurisaddaireyesjurez/pandas"
+      descripcion: "Certificado por la realizaciòn del curso PANDAS: https://www.kaggle.com/learn/certification/zurisaddaireyesjurez/pandas"
     };
 
     let cert8 = {
       titulo: "Intro to Machine Learning",
-      descripcion: "https://www.kaggle.com/learn/certification/zurisaddaireyesjurez/intro-to-machine-learning"
+      descripcion: "Certificado por la realizaciòn del curso INTRO TO MACHINE LEARNING: https://www.kaggle.com/learn/certification/zurisaddaireyesjurez/intro-to-machine-learning"
+    };
+
+    let cert9 = {
+      titulo: "Intro to Deep Learning",
+      descripcion: "Certificado por la realizaciòn del curso INTRO TO DEEP LEARNING: https://www.kaggle.com/learn/certification/zurisaddaireyesjurez/intro-to-deep-learning"
     };
 
 
@@ -60,6 +65,7 @@ export class CertificatesComponent implements OnInit {
     this.certificates.push(cert6);
     this.certificates.push(cert7);
     this.certificates.push(cert8);
+    this.certificates.push(cert9);
 
 
   }
